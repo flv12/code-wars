cada31eecd0f3bfe9dc446345f809e9fd41ff66e
flv12 <26801813+flv12@users.noreply.github.com>
2024-07-30 19:58:35 +0200

    TU ajoutés pour SumStringsAsNumbers

---
+++ sum-strings-as-numbers/src/SumStringsAsNumbers.php
@@ -0,0 +1,13 @@
+<?php
+
+class SumStringsAsNumbers
+{
+    public static function sumStrings(string $a, string $b): string
+    {
+    }
+}
+
+function sum_strings(string $a, string $b): string
+{
+    return SumStringsAsNumbers::sumStrings($a, $b);
+}
